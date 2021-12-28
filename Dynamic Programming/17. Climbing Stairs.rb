7156744e056c1801f1cca8ed7b0908c2478cdc05
Glenn Espinosa <glennpeter.espinosa@gmail.com>
2021-12-28 00:11:39 -0500

    Implement Climbing Stairs bottom up approach

--- Dynamic Programming/17. Climbing Stairs.rb
+++ Dynamic Programming/17. Climbing Stairs.rb
@@ -1,28 +1,28 @@
 require 'pry'
 
+# def fib_bottom_up(n)
+#   result = [0, 1, 1]
+
+#   while result.length <= n
+#     result << result[-1] + result[-2]
+#   end
+
+#   result[n]
+# end
+
 def climb_stairs(n)
-  # current_x = 0
+  result = [1,2]
 
-  # n.times do |x|
-  #   current_x = climb_stairs(x - 1) + x
-
-  #   if current_x < x
-  #     current_x = x
-  #   end
-  # end
-
-  # current_x
-  # binding.pry
-  one, two = 1, 1
-
-  (n - 1).times do
-    temp = one
-    one = one + two
-    two = temp
+  while result.length <= n
+    result << result[-1] + result[-2]
   end
 
-  one
+  result[n-1]
 end
 
 p climb_stairs(2) == 2
 p climb_stairs(3) == 3
+
+# p fib_bottom_up(4)
+# p fib_bottom_up(5)
+# p fib_bottom_up(1000)
