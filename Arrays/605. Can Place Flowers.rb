1d039d5f8e8ab58e2ad793b07dcd5166da19f2c2
Glenn Espinosa <glennpeter.espinosa@gmail.com>
2022-01-08 02:43:13 -0500

    Implement 605. Can Place Flowers

--- Arrays/605. Can Place Flowers.rb
+++ Arrays/605. Can Place Flowers.rb
@@ -4,13 +4,25 @@ def can_place_flowers(flowerbed, n)
   return false if flowerbed.nil? || flowerbed.empty?
   return true if n.nil? || n <= 0
 
+  length = flowerbed.length - 1
   count = 0
 
-  flowerbed.each do |flower|
-    count += 1 if flower == 0
+  flowerbed.each_with_index do |flower, idx|
+    if flower == 0
+      if flowerbed[idx-1] == 0 && flowerbed[idx + 1] == 0
+        flowerbed[idx] = 1
+        count += 1
+      elsif idx == 0 && flowerbed[idx + 1] == 0
+        count +=  1
+      elsif idx == length && flowerbed[idx - 1] == 0
+        count += 1
+      else
+        next
+      end
+    end
   end
 
-  (count / 2) >= n
+  count >= n
 end
 
 p can_place_flowers([1,0,0,0,1], 2) == false
