a3057f7690b2b8c0db057ce160d908a6061750f6
Glenn Espinosa <glennpeter.espinosa@gmail.com>
2022-01-17 22:28:03 -0500

    Implement 219. Contains Duplicate II

--- 219. Contains Duplicate II.rb
+++ 219. Contains Duplicate II.rb
@@ -2,18 +2,14 @@ require 'pry'
 
 def contains_nearby_duplicate(nums, k)
   return false if nums.empty? || nums.nil?
+  return false if k.nil?
 
   nums.each_with_index.inject({}) do |memo, (num, idx)|
-    if memo[num].nil?
-      memo[num] = [idx]
-    else
-      memo[num].each do |jidx|
-        return true if (idx - jidx).abs <= k
-      end
-
-      memo[num] << idx
+    memo[num] ||= []
+    memo[num].each do |jidx|
+      return true if (idx - jidx).abs <= k
     end
-
+    memo[num] << idx
     memo
   end
 
