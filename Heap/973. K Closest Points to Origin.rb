4e39da143e66dfd5b40c6324e2ba41b237d09274
Glenn Espinosa <glennpeter.espinosa@gmail.com>
2022-01-23 02:32:42 -0500

    Add  O(n log k) solution for K Closest Points to Origin

--- Heap/973. K Closest Points to Origin.rb
+++ Heap/973. K Closest Points to Origin.rb
@@ -1,5 +1,6 @@
 require 'pry'
 
+# O(n log n)
 def k_closest(points, k)
   return [] if points.empty? || points.nil?
   return StandardError if k.nil?
@@ -16,8 +17,44 @@ def euclidean_distance(x, y)
   Math.sqrt((x * x) + (y * y)).abs
 end
 
+class Point
+  attr_accessor :x, :y, :val
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+    @val = [x,y]
+  end
+
+  def <=>(other)
+    euclidean_distance <=> other.euclidean_distance
+  end
+
+  def euclidean_distance
+    Math.sqrt((x * x) + (y * y))
+  end
+end
+
+# O(n log k)
+def k_closest(points, k)
+  return [] if points.empty? || points.nil?
+  return StandardError if k.nil?
+
+  result = []
+
+  points.each do |point|
+    point = Point.new(point[0], point[1])
+
+    result << point
+
+    if result.length > k
+      result.sort!
+      result.pop
+    end
+  end
+
+  result.map(&:val)
+end
+
 points = [[1,3],[-2,2]]
 p k_closest(points, 1) == [[-2,2]]
-
-points =
-[[2,2],[2,2],[3,3],[2,-2],[1,1]]
