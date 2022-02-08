8b43b95165a6726cc7067553ba767a11e5b0e082
Glenn Espinosa <glennpeter.espinosa@gmail.com>
2022-02-07 21:23:17 -0500

    Implement 236. Lowest Common Ancestor of a Binary Tree

--- Graph/145. Binary Tree Postorder Traversal.rb
+++ Graph/145. Binary Tree Postorder Traversal.rb
@@ -19,3 +19,21 @@ def postorder_traversal(root)
 
   result.reverse
 end
+
+
+def postorder_traversal(root)
+  return [] if root.nil?
+
+  result = []
+
+  dfs(root, result)
+end
+
+def dfs(node, result)
+  return result if node.nil?
+
+  dfs(node.left, result)
+  dfs(node.right, result)
+
+  result << node.val
+end
