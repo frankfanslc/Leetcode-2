33ba0a328faf08c8d1bcb85f7432842b08fc70da
Glenn Espinosa <glennpeter.espinosa@gmail.com>
2022-02-10 17:33:50 -0500

    Implement 243. Shortest Word Distance optimally

--- Arrays/243. Shortest Word Distance.rb
+++ Arrays/243. Shortest Word Distance.rb
@@ -1,36 +1,45 @@
 require 'pry'
 
-def shortest_distance(words_dict, word1, word2)
-  return StandardError if words_dict.nil? || words_dict.empty?
-  return StandardError if word1.nil? || word2.nil?
+def shortest_distance(words, word1, word2)
+  return -1 if words.empty?
 
-  hash = Hash.new
-  words_dict.each_with_index do |word, idx|
-    if word == word1 or word == word2
-      hash[word] ||= []
-      hash[word] << idx
+  word1_idx = nil
+  word2_idx = nil
+  min_distance = Float::INFINITY
+
+  words.each_with_index do |word, idx|
+    if word == word1
+      word1_idx = idx
+
+      if word1_idx && word2_idx
+        distance = (word1_idx - word2_idx).abs
+        min_distance = [min_distance, distance].min
+      end
+    end
+
+    if word == word2
+      word2_idx = idx
+
+      if word1_idx && word2_idx
+        distance = (word2_idx - word1_idx).abs
+        min_distance = [min_distance, distance].min
+      end
     end
   end
 
-  shortest_distance = hash.values.flatten.max
-  hash.values[0].each do |word1|
-    hash.values[1].each do |word2|
-      distance1 = word1 - word2
-      distance2 = word2 - word1
+  return -1 if min_distance == Float::INFINITY
 
-      if distance1 > 0 && distance1 < shortest_distance
-        shortest_distance = distance1
-      end
-
-      if distance2 > 0 && distance2 < shortest_distance
-        shortest_distance = distance2
-      end
-    end
-  end
-
-  shortest_distance
+  min_distance
 end
 
 words_dict = ["practice", "practice", "makes", "perfect", "coding", "makes"]
-output = 3
-p shortest_distance(words_dict, 'coding', 'practice')
+p shortest_distance(words_dict, 'coding', 'practice') == 3
+
+words_dict = ["practice", "practice", "makes", "perfect", "leet", "makes", "coding"]
+p shortest_distance(words_dict, 'coding', 'practice') == 5
+
+words_dict = ["practice", "practice", "coding", "perfect", "leet", "makes", "coding"]
+p shortest_distance(words_dict, 'coding', 'practice') == 1
+
+words_dict = ["practice", "practice", "coding", "perfect", "leet", "makes", "coding"]
+p shortest_distance(words_dict, 'coding', 'sike') == -1
