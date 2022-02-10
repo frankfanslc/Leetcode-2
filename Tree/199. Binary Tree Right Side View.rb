1ad90ef3a594caef579e5289f74cdc037c877bcb
Glenn Espinosa <glennpeter.espinosa@gmail.com>
2022-02-09 21:22:38 -0500

    Implement 199. Binary Tree Right Side View

--- Tree/199. Binary Tree Right Side View.rb
+++ Tree/199. Binary Tree Right Side View.rb
@@ -3,33 +3,23 @@ require 'pry'
 def right_side_view(root)
   return [] if root.nil?
 
-  queue = [[root, true]]
-  result = []
+  queue = [root, :end_level]
+  result = [root.val]
 
   while queue.any?
-    current = queue.pop
-    current_node = current[0]
-    right_side_visible = current[1]
+    current = queue.shift
 
-    if right_side_visible
-      result << current_node.val
+    if current == :end_level
+      if queue.any?
+        result << queue.last.val
+        queue << :end_level
+      end
+      next
     end
 
-    if current_node.right || has_right_node_to_come?(queue)
-      queue.prepend([current_node.left, false]) if current_node.left
-    else
-      queue.prepend([current_node.left, true])  if current_node.left
-    end
-
-    queue.prepend([current_node.right, true]) if current_node.right
-
+    queue << current.left  if current.left
+    queue << current.right if current.right
   end
 
   result
-end
-
-def has_right_node_to_come?(queue)
-  queue.find do |val|
-    val[0] == true
-  end
 end
