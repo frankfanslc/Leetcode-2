34d4ca7e71baaf1768fee450dbce04c6e5825f22
Glenn Espinosa <glennpeter.espinosa@gmail.com>
2022-02-14 14:58:28 -0500

    Implement 286. Walls and Gates

--- Graph/286. Walls and Gates.rb
+++ Graph/286. Walls and Gates.rb
@@ -1,7 +1,54 @@
 require 'pry'
 
-def walls_and_gates(rooms)
+EMPTY_ROOM = (2**31 - 1)
+GATE = 0
 
+def walls_and_gates(rooms)
+  return rooms if rooms.nil? || rooms.empty?
+
+  gates_queue = []
+
+  rooms.each_with_index do |row, ridx|
+    row.each_with_index do |col, cidx|
+      gates_queue << [ridx, cidx] if col == GATE
+    end
+  end
+
+  current_distance = 1
+  gates_queue << :increment
+
+  while gates_queue.any?
+    current = gates_queue.shift
+
+    if current == :increment
+      if gates_queue.empty?
+        return rooms
+      else
+        gates_queue << :increment
+        current_distance += 1
+        next
+      end
+    end
+
+    adjacent_rooms = adjacent_rooms(rooms, current[0], current[1])
+    adjacent_rooms.each do |room|
+      next if rooms[room[0]][room[1]] != EMPTY_ROOM
+
+      rooms[room[0]][room[1]] = current_distance
+      gates_queue << room
+    end
+  end
+
+  rooms
+end
+
+def adjacent_rooms(rooms, row, col)
+  cells = []
+  cells << [row-1, col] if row > 0
+  cells << [row, col+1] if col < rooms[0].length - 1
+  cells << [row+1, col] if row < rooms.length - 1
+  cells << [row, col-1] if col > 0
+  cells
 end
 
 rooms = [[2147483647,-1,0,2147483647],
@@ -12,4 +59,4 @@ output = [[3,-1,0,1],
           [2,2,1,-1],
           [1,-1,2,-1],
           [0,-1,3,4]]
-p walls_and_gates(rooms)
+p walls_and_gates(rooms) == output
